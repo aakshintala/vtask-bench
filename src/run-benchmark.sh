151e4f672b35f0b5d5d3a7611edfdbdb86e96a6f
Amogh Akshintala <aakshintala@cs.unc.edu>
2020-09-30 21:31:17 -0400

    Use the binary output flag and append to a single file

--- src/run-benchmark.sh
+++ src/run-benchmark.sh
@@ -3,14 +3,13 @@ set -x #echo on
 
 # Make a new directory for each run.
 topdir=$(pwd)
-benchmarkDir=$topdir"/benchmark-$(date +%Y-%m-%d-%H-%M-%S)"
-mkdir $benchmarkDir
-cd $benchmarkDir
+outfile=$topdir"/vtask-bench-out-$(date +%Y-%m-%d-%H-%M-%S)"
+touch outfile
 
 # the benchmark uses ints, so divide by sizeof(int)
 # Tune this according to max memory on your GPU.
-# GTX 970 I use has 4GiB so I set this to 2GiB
-let totalDataMoved=2*1024*1024*1024/4;
+# GTX 970 I use has 4GiB so I set this to 1GiB
+let totalDataMoved=1*1024*1024*1024/4;
 
 # Because each element is a 32-bit int, that's 4kb (1 page)
 let elemSize=1024;
@@ -20,11 +19,7 @@ while [[ $elemSize -lt 1073741824 ]]; do
 	let numElems=$totalDataMoved/$elemSize;
 	let computeTime=1; # Time in microseconds that simulates 'work' Range 10us to 10ms
 	while [[ $computeTime -lt 10000 ]]; do # stop at 1 milliseconds
-		subDir=$numElems-$elemSize-$computeTime
-		mkdir $subDir
-		cd $subDir
-		sudo timeout 6000 $topdir/vtask-bench -q $numElems -s $elemSize -t $computeTime | tee timing
-		cd ..
+		sudo timeout 6000 $topdir/vtask-bench -m -q $numElems -s $elemSize -t $computeTime | tee -a outfile
 		let computeTime=$computeTime*10;
 	done
 	let elemSize=$elemSize*4;
